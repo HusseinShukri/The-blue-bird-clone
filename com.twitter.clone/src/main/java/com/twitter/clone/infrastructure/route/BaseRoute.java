24dba3d04849b9865ab8dfba51d023ce8304137b
HusseinShukri <31711344+HusseinShukri@users.noreply.github.com>
2023-08-24 03:13:09 +0300

    Add packagePath annotation and adjust the code

--- com.twitter.clone/src/main/java/com/twitter/clone/infrastructure/route/BaseRoute.java
+++ com.twitter.clone/src/main/java/com/twitter/clone/infrastructure/route/BaseRoute.java
@@ -1,10 +1,13 @@
 package com.twitter.clone.infrastructure.route;
 
 import com.twitter.clone.infrastructure.annotation.route.Http;
+import com.twitter.clone.infrastructure.annotation.route.PackagePath;
 import com.twitter.clone.infrastructure.annotation.route.RouteController;
 import io.javalin.apibuilder.ApiBuilder;
 import io.javalin.apibuilder.EndpointGroup;
+import org.jetbrains.annotations.NotNull;
 import org.reflections.Reflections;
+
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,46 +23,57 @@ public abstract class BaseRoute implements EndpointGroup {
 
     protected abstract void initializeControllerInstances();
 
-    protected abstract String getModuleName();
-
     @Override
     public void addEndpoints() {
         initializeControllerInstances();
 
-        Reflections reflections = new Reflections(BASE_PACKAGE+getModuleName());
+        Reflections reflections = new Reflections(BASE_PACKAGE + getPackagePath());
         Set<Class<?>> controllerClasses = reflections.getTypesAnnotatedWith(RouteController.class);
 
         for (Class<?> controllerClass : controllerClasses) {
-            try {
-                RouteController controllerAnnotation = controllerClass.getAnnotation(RouteController.class);
-                String controllerPath = BASE_PATH + PATH_SEPARATE + controllerAnnotation.value();
-
-                registerControllerRoutes(controllerClass, controllerPath, controllerInstancesMap);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            RouteController controllerAnnotation = controllerClass.getAnnotation(RouteController.class);
+            String controllerPath = getControllerPath(controllerAnnotation);
+            registerControllerRoutes(controllerClass, controllerPath, controllerInstancesMap);
         }
+    }
+
+    @NotNull
+    private static String getControllerPath(RouteController controllerAnnotation) {
+        String controllerPath = BASE_PATH + PATH_SEPARATE + controllerAnnotation.value();
+        return controllerPath;
+    }
+
+    @NotNull
+    private String getPackagePath() {
+        PackagePath packagePath = this.getClass().getAnnotation(PackagePath.class);
+        if (packagePath == null) {
+            throw new RuntimeException("Classes extending BaseRoute must be annotated with @BasePackageConfig");
+        }
+        return packagePath.value();
     }
 
     protected void registerControllerRoutes(Class<?> controllerClass, String controllerPath, Map<Class<?>, Object> controllerInstancesMap) {
         Object controllerInstance = controllerInstancesMap.get(controllerClass);
-
         if (controllerInstance == null) {
             throw new RuntimeException("No instance found for controller: " + controllerClass.getName());
         }
 
         path(controllerPath + PATH_SEPARATE, () -> {
             for (Method method : controllerClass.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(Http.Get.class)) {
-                    registerGet(method, controllerInstance);
-                } else if (method.isAnnotationPresent(Http.Post.class)) {
-                    registerPost(method, controllerInstance);
-                }else if (method.isAnnotationPresent(Http.Put.class)) {
-                    registerPut(method, controllerInstance);
-                }else if (method.isAnnotationPresent(Http.Patch.class)) {
-                    registerPatch(method, controllerInstance);
-                }else if (method.isAnnotationPresent(Http.Delete.class)) {
-                    registerDelete(method, controllerInstance);
+                try {
+                    if (method.isAnnotationPresent(Http.Get.class)) {
+                        registerGet(method, controllerInstance);
+                    } else if (method.isAnnotationPresent(Http.Post.class)) {
+                        registerPost(method, controllerInstance);
+                    } else if (method.isAnnotationPresent(Http.Put.class)) {
+                        registerPut(method, controllerInstance);
+                    } else if (method.isAnnotationPresent(Http.Patch.class)) {
+                        registerPatch(method, controllerInstance);
+                    } else if (method.isAnnotationPresent(Http.Delete.class)) {
+                        registerDelete(method, controllerInstance);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
@@ -68,55 +82,35 @@ public abstract class BaseRoute implements EndpointGroup {
     protected void registerGet(Method method, Object controllerInstance) {
         Http.Get get = method.getAnnotation(Http.Get.class);
         ApiBuilder.get(get.value(), ctx -> {
-            try {
-                method.invoke(controllerInstance, ctx);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            method.invoke(controllerInstance, ctx);
         });
     }
 
     protected void registerPost(Method method, Object controllerInstance) {
         Http.Post post = method.getAnnotation(Http.Post.class);
         ApiBuilder.post(post.value(), ctx -> {
-            try {
-                method.invoke(controllerInstance, ctx);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            method.invoke(controllerInstance, ctx);
         });
     }
 
     protected void registerPut(Method method, Object controllerInstance) {
         Http.Put put = method.getAnnotation(Http.Put.class);
         ApiBuilder.put(put.value(), ctx -> {
-            try {
-                method.invoke(controllerInstance, ctx);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            method.invoke(controllerInstance, ctx);
         });
     }
 
     protected void registerPatch(Method method, Object controllerInstance) {
         Http.Patch patch = method.getAnnotation(Http.Patch.class);
         ApiBuilder.patch(patch.value(), ctx -> {
-            try {
-                method.invoke(controllerInstance, ctx);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            method.invoke(controllerInstance, ctx);
         });
     }
 
     protected void registerDelete(Method method, Object controllerInstance) {
         Http.Delete delete = method.getAnnotation(Http.Delete.class);
         ApiBuilder.delete(delete.value(), ctx -> {
-            try {
-                method.invoke(controllerInstance, ctx);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            method.invoke(controllerInstance, ctx);
         });
     }
 }
--- com.twitter.clone/src/main/java/com/twitter/clone/tweet/infrastructure/route/TweetRoute.java
+++ com.twitter.clone/src/main/java/com/twitter/clone/tweet/infrastructure/route/TweetRoute.java
@@ -1,10 +1,12 @@
 package com.twitter.clone.tweet.infrastructure.route;
 
 import com.google.inject.Inject;
+import com.twitter.clone.infrastructure.annotation.route.PackagePath;
 import com.twitter.clone.infrastructure.route.BaseRoute;
 import com.twitter.clone.tweet.api.controller.TweetController;
 import lombok.RequiredArgsConstructor;
 
+@PackagePath("tweet")
 @RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class TweetRoute extends BaseRoute {
     private final TweetController tweetController;
@@ -12,10 +14,5 @@ public class TweetRoute extends BaseRoute {
     @Override
     protected void initializeControllerInstances() {
         super.controllerInstancesMap.put(TweetController.class, tweetController);
-    }
-
-    @Override
-    protected String getModuleName() {
-        return "tweet";
     }
 }
