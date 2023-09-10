4a1996eaad4455b5180364bdef2bcbd887b8c702
HusseinShukri <31711344+HusseinShukri@users.noreply.github.com>
2023-09-10 03:57:34 +0300

    Add validation for authentication mappers

--- com.twitter.clone/src/main/java/com/twitter/clone/authentication/api/mapper/AuthenticationMapper.java
+++ com.twitter.clone/src/main/java/com/twitter/clone/authentication/api/mapper/AuthenticationMapper.java
@@ -6,18 +6,39 @@ import io.javalin.http.Context;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class AuthenticationMapper {
 
     private final ModelMapper mapper;
 
-    public AuthenticationDto.LoginDto contextToLoginDto(Context context){
-        if (context == null) return null;
-        return new AuthenticationDto.LoginDto(context.formParam("email"),context.formParam("password"));
+    public AuthenticationDto.LoginDto contextToLoginDto(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("Context cannot be null");
+        }
+
+        String email = Optional.ofNullable(context.formParam("email"))
+                .orElseThrow(() -> new IllegalArgumentException("Email is required"));
+        String password = Optional.ofNullable(context.formParam("password"))
+                .orElseThrow(() -> new IllegalArgumentException("Password is required"));
+
+        return new AuthenticationDto.LoginDto(email, password);
     }
 
-    public AuthenticationDto.SignupDto contextToSignupDto(Context context){
-        if (context == null) return null;
-        return new AuthenticationDto.SignupDto(context.formParam("email"), context.formParam("username"),context.formParam("password"));
+    public AuthenticationDto.SignupDto contextToSignupDto(Context context) {
+        if (context == null) {
+            throw new IllegalArgumentException("Context cannot be null");
+        }
+
+        String email = Optional.ofNullable(context.formParam("email"))
+                .orElseThrow(() -> new IllegalArgumentException("Email is required"));
+        String username = Optional.ofNullable(context.formParam("username"))
+                .orElseThrow(() -> new IllegalArgumentException("Username is required"));
+        String password = Optional.ofNullable(context.formParam("password"))
+                .orElseThrow(() -> new IllegalArgumentException("Password is required"));
+
+        return new AuthenticationDto.SignupDto(email, username, password);
     }
+
 }
